36a73221d2412dcf939a93860c8669abfd8ecc60
drizzlefizzzax <chrisrankin2801@gmail.com>
2020-08-16 17:05:17 +0100

    Updates

---
+++ precache-manifest.61b854067f5bbf08629f575580f5bd52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebce0a7777cb22bbce9a530a3e5b70f",
+    "url": "/olympic-plate-calculator/index.html"
+  },
+  {
+    "revision": "983d8cebf0bb3c0f62cc",
+    "url": "/olympic-plate-calculator/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9d699e539129e6849723",
+    "url": "/olympic-plate-calculator/static/js/2.f3016b3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/olympic-plate-calculator/static/js/2.f3016b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983d8cebf0bb3c0f62cc",
+    "url": "/olympic-plate-calculator/static/js/main.51b10e27.chunk.js"
+  },
+  {
+    "revision": "e7920fc8bd1d0b85ada6",
+    "url": "/olympic-plate-calculator/static/js/runtime-main.6796bc32.js"
+  }
+]);
